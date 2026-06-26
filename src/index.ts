2151ddf269b200fd3b2c8937e216268022c42f17
Francisco Perez <francisco@example.com>
2026-06-26 17:56:50 -0300

    primer commit zeni

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+﻿import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_, res) => {
+    res.json({
+        proyecto: "ZENI",
+        estado: "OK",
+        mensaje: "Servidor funcionando"
+    });
+});
+
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 ZENI iniciado en http://localhost:${PORT}`);
+});
